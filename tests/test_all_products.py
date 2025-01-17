a8e88d9b3b2b9e03bd9765c25514b8d070ab7c3e
Raed Eleyan <raedeleyan1@gmail.com>
2025-01-17 15:21:03 +0200

    Add some improvements to test_all_products test file

--- tests/test_all_products.py
+++ tests/test_all_products.py
@@ -10,82 +10,129 @@ The tests cover various scenarios, including:
 @date: 01/17/2025
 @contact: raedeleyan1@gmail.com
 """
+from utils.endpoints_loader import EndpointsLoader
+from utils.api_client import APIClient
 
 
-def test_get_all_products_no_parameters(endpoints_loader, api_client):
-    """
-    Test the get_all_products endpoint without any parameters.
-    """
-    products_endpoint = endpoints_loader.get_endpoint('get_all_products')
-    expected_status_code = products_endpoint.get('status_code').get('ok')['code']
-    response_body, status_code = api_client.send_request(method=products_endpoint.get('method'),
-                                                         endpoint=products_endpoint.get('endpoint'))
+class TestAllProducts:
+    PRODUCTS_ENDPOINT: str = 'get_all_products'
+    OK_STATUS: str = 'ok'
+    BAD_REQUEST_STATUS: str = 'bad_request'
 
-    # Validate the status code
-    api_client.validate_status_code(expected_status_code=expected_status_code, actual_status_code=status_code)
+    def test_get_all_products_no_parameters(self, endpoints_loader: EndpointsLoader, api_client: APIClient) -> None:
+        """
+        Test the get_all_products endpoint without any parameters.
 
-    # Validate the response body structure
-    assert isinstance(response_body, list), "Response body should be a list of products"
-    for product in response_body:
-        assert "id" in product, "Product should have an 'id' field"
-        assert "category" in product, "Product should have a 'category' field"
-        assert "name" in product, "Product should have a 'name' field"
-        assert "inStock" in product, "Product should have an 'inStock' field"
+        :param endpoints_loader: Fixture to load endpoint configurations.
+        :param api_client: Fixture to initialize the APIClient.
+        """
+        products_endpoint = self._get_products_endpoint(endpoints_loader)
+        expected_status_code = self._get_expected_status_code(products_endpoint)
+        response_body, status_code = api_client.send_request(
+            method=products_endpoint.get('method'),
+            endpoint=products_endpoint.get('endpoint')
+        )
 
+        # Validate the status code
+        api_client.validate_status_code(expected_status_code=expected_status_code, actual_status_code=status_code)
 
-def test_get_all_products_with_category(endpoints_loader, api_client):
-    """
-    Test the get_all_products endpoint with the 'category' parameter.
-    """
-    products_endpoint = endpoints_loader.get_endpoint('get_all_products')
-    expected_status_code = products_endpoint.get('status_code').get('ok')['code']
-    response_body, status_code = api_client.send_request(method=products_endpoint.get('method'),
-                                                         endpoint=products_endpoint.get('endpoint'),
-                                                         params={"category": "coffee"})
-
-    # Validate the status code
-    api_client.validate_status_code(expected_status_code=expected_status_code, actual_status_code=status_code)
-
-    # Validate the response body
-    assert isinstance(response_body, list), "Response body should be a list of products"
-    for product in response_body:
-        assert "id" in product, "Product should have an 'id' field"
-        assert product["category"] == "coffee", "All products should belong to the 'coffee' category"
-        assert "name" in product, "Product should have a 'name' field"
-        assert "inStock" in product, "Product should have an 'inStock' field"
+        # Validate the response body structure
+        assert isinstance(response_body, list), "Response body should be a list of products"
+        for product in response_body:
+            assert "id" in product, "Product should have an 'id' field"
+            assert "category" in product, "Product should have a 'category' field"
+            assert "name" in product, "Product should have a 'name' field"
+            assert "inStock" in product, "Product should have an 'inStock' field"
 
 
-def test_get_all_products_with_multiple_parameters(endpoints_loader, api_client):
-    """
-    Test the get_all_products endpoint with multiple parameters.
-    """
-    products_endpoint = endpoints_loader.get_endpoint('get_all_products')
-    expected_status_code = products_endpoint.get('status_code').get('ok')['code']
-    response_body, status_code = api_client.send_request(
-        method=products_endpoint.get('method'),
-        endpoint=products_endpoint.get('endpoint'),
-        params={"category": "coffee", "results": 2, "available": "true"})
+    def test_get_all_products_with_category(self, endpoints_loader: EndpointsLoader, api_client: APIClient) -> None:
+        """
+        Test the get_all_products endpoint with the 'category' parameter.
 
-    # Validate the status code
-    api_client.validate_status_code(expected_status_code=expected_status_code, actual_status_code=status_code)
+        :param endpoints_loader: Fixture to load endpoint configurations.
+        :param api_client: Fixture to initialize the APIClient.
+        """
+        products_endpoint = self._get_products_endpoint(endpoints_loader)
+        expected_status_code = self._get_expected_status_code(products_endpoint)
+        response_body, status_code = api_client.send_request(
+            method=products_endpoint.get('method'),
+            endpoint=products_endpoint.get('endpoint'),
+            params={"category": "coffee"}
+        )
 
-    # Validate the response body structure
-    assert isinstance(response_body, list), "Response body should be a list of products"
-    assert len(response_body) <= 2, "Response should contain at most 2 products"
-    for product in response_body:
-        assert "id" in product, "Product should have an 'id' field"
-        assert product["category"] == "coffee", "All products should belong to the 'coffee' category"
-        assert "name" in product, "Product should have a 'name' field"
-        assert product["inStock"] is True, "All products should be in stock"
+        # Validate the status code
+        api_client.validate_status_code(expected_status_code=expected_status_code, actual_status_code=status_code)
 
-def test_get_all_products_invalid_results(endpoints_loader, api_client):
-    """
-    Test the get_all_products endpoint with an invalid 'results' parameter.
-    """
-    products_endpoint = endpoints_loader.get_endpoint('get_all_products')
-    expected_status_code = products_endpoint.get('status_code').get('bad_request')['code']
-    response_body, status_code = api_client.send_request(
-        method=products_endpoint.get('method'),
-        endpoint=products_endpoint.get('endpoint'),
-        params={"results": -5})
-    api_client.validate_status_code(expected_status_code=expected_status_code, actual_status_code=status_code)
+        # Validate the response body
+        assert isinstance(response_body, list), "Response body should be a list of products"
+        for product in response_body:
+            assert "id" in product, "Product should have an 'id' field"
+            assert product["category"] == "coffee", "All products should belong to the 'coffee' category"
+            assert "name" in product, "Product should have a 'name' field"
+            assert "inStock" in product, "Product should have an 'inStock' field"
+
+
+    def test_get_all_products_with_multiple_parameters(self, endpoints_loader: EndpointsLoader, api_client: APIClient) -> None:
+        """
+        Test the get_all_products endpoint with multiple parameters.
+
+        :param endpoints_loader: Fixture to load endpoint configurations.
+        :param api_client: Fixture to initialize the APIClient.
+        """
+        products_endpoint = self._get_products_endpoint(endpoints_loader)
+        expected_status_code = self._get_expected_status_code(products_endpoint)
+        response_body, status_code = api_client.send_request(
+            method=products_endpoint.get('method'),
+            endpoint=products_endpoint.get('endpoint'),
+            params={"category": "coffee", "results": 2, "available": "true"}
+        )
+
+        # Validate the status code
+        api_client.validate_status_code(expected_status_code=expected_status_code, actual_status_code=status_code)
+
+        # Validate the response body structure
+        assert isinstance(response_body, list), "Response body should be a list of products"
+        assert len(response_body) <= 2, "Response should contain at most 2 products"
+        for product in response_body:
+            assert "id" in product, "Product should have an 'id' field"
+            assert product["category"] == "coffee", "All products should belong to the 'coffee' category"
+            assert "name" in product, "Product should have a 'name' field"
+            assert product["inStock"] is True, "All products should be in stock"
+
+    def test_get_all_products_invalid_results(self, endpoints_loader: EndpointsLoader, api_client: APIClient) -> None:
+        """
+        Test the get_all_products endpoint with an invalid 'results' parameter.
+
+        :param endpoints_loader: Fixture to load endpoint configurations.
+        :param api_client: Fixture to initialize the APIClient.
+        """
+        products_endpoint = self._get_products_endpoint(endpoints_loader)
+        expected_status_code = self._get_expected_status_code(products_endpoint)
+        response_body, status_code = api_client.send_request(
+            method=products_endpoint.get('method'),
+            endpoint=products_endpoint.get('endpoint'),
+            params={"results": -5}
+        )
+
+        # Validate the status code
+        api_client.validate_status_code(expected_status_code=expected_status_code, actual_status_code=status_code)
+
+    def _get_products_endpoint(self, endpoints_loader: EndpointsLoader) -> dict:
+        """
+        Helper method to get the products endpoint configuration.
+
+        :param endpoints_loader: Fixture to load endpoint configurations.
+        :return: The products endpoint configuration.
+        """
+        return endpoints_loader.get_endpoint(self.PRODUCTS_ENDPOINT)
+
+    @staticmethod
+    def _get_expected_status_code(products_endpoint: dict, status_type: str = OK_STATUS) -> int:
+        """
+        Helper method to get the expected status code.
+
+        :param products_endpoint: The products endpoint configuration.
+        :param status_type: The type of status code to retrieve (e.g., "ok", "bad_request").
+        :return: The expected status code.
+        """
+        return products_endpoint.get('status_code').get(status_type)['code']
